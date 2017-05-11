449cb12a214e130f63ebf2082cce24f743321f95
Bogdan <bogdan.tabusca@gmail.com>
2017-05-11 22:09:22 +0300

    Implemented the Pre-Decorator -in asteptarea informattilor primite de la querry -creaza casute cu informatiile primite de la server -very preety

--- WebBack/Main.py
+++ WebBack/Main.py
@@ -1,23 +1,18 @@
 import logging
-import Cookie
+import mimetypes
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from DBController   import DBConnection
 from os import curdir, sep, path
-
-
+from random import randint
+#Cookie (lib)
+#Cookie=Cookie.Simple=Cookie(.self.headers["Cookies"])
+#vezi documentatie base hhtp server
+#for c_in headers:
+#send_header("set.cookie.p")..
+#js -ajax -> //cauta
+#jason dump
+#response.header=["set-cookie"]="s_id"=1234";
 class AppHandler(BaseHTTPRequestHandler):
-
-    db_conn = DBConnection.connect("PROJECT1", "project1", "localhost")
-
-    def format_path(self):
-        if self.path == "/":
-            self.path = "/index.html"
-
-        self.path =  "../WebFront" + self.path
-
-        return path.splitext(self.path)
-
-
 
     def do_GET(self):
         content_type = {
@@ -34,52 +29,45 @@ class AppHandler(BaseHTTPRequestHandler):
         }
 
 
+        print self.path
 
-        fpath, ext =  self.format_path() # Splits the given string into the simple path and the extension of the final file(including the lading dot, ".html")
+        if self.path == "/":
+            self.path = "/index.html"
+        if self.path == "favico.ico":
+            return
+
+        self.path =  "../WebFront" + self.path
+
+        fname, ext = path.splitext(self.path) # Splits the given string into the simple path and the extension of the final file(including the lading dot, ".html"")
         if ext in content_type.keys():
             with open(self.path, 'rb') as f:
                 self.send_response(200)
                 self.send_header('Content-type', content_type[ext])
                 self.end_headers()
-
                 self.wfile.write(f.read())
         return
 
+    def do_HEAD(self):
+        logging.debug("HEAD")
+
     def do_POST(self):
         print "POST"
-        form = str(self.rfile.read(int(self.headers['Content-Length']))) # Read the message as a string
+        recived=str(self.rfile.read(int(self.headers['Content-Length'])))
+        print recived
+        if 'UsernameBox' in recived:
+            form = recived.split('&')
+            for f in form:
+              f = [f[:f.find("=")], f[f.find("=")+1:]]
 
-        if form.find("item") > 0:
-            result = self.db_conn.execute("select title from items") # ce select vrei tu
-            # acum result e o matrice bidimensionala. Daca ai result[row][cell], row va fi index al randului pe care il
-            # vrei, iar cell va fi index-ul coloanei din tabel pe care o vrei. Ambii indecsi incep de la 0, nu de la 1
-            # Ex. result[0][2] imi va lua primul rand, si a treia coloana. Daca le vrei ca str faci str(result[x][y])
+              print f[1]
+        else:
+            t=randint(0,9000);
 
-        # for f in form.split('&'):
-        #     f = [f[:f.find("=")], f[f.find("=")+1:]]
-        #     print f
-
-
-
-
-    def register_attempt(self, form):
-        if form[0][0] == "Username" and form[1][0] == "Password" and form[2][0] == "Confirm" and form[3][0] == "Email":
-            return True
-
-    def register(self, form):
-        for c in form[:][:2]:
-            if c not in "qwertyuiopasdfghjklzxcvbnm0123456789._":
-                return "Invalid characters.\nOnly alphanumeric characters, plus '.' and '_' are allowed."
-
-
-
-
-        # Char reconstruction, to be added later
-        # char_list = {'%20' : ' ',}
-        #
-        # for f in form:
-        #     for c in char_list.keys():
-        #         f.replace(c, char_list[c])
+            self.send_response(200)
+            self.send_header("content-type","text/html")
+            self.end_headers()
+            self.wfile.write("An Item!"+"Here is a Random Generated Code YeaH!"+str(t))#jason.dumps(content)
+            return
 
 
 
@@ -88,6 +76,5 @@ def run(server=HTTPServer, handler=AppHandler, port=2526):
     httpd = server(server_address, handler)
     print "Started the HTTP Server at port", port
     httpd.serve_forever()
-
 
 run()
