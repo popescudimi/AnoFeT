4e32363e7e0f74e5e9a2298c860d9fb59ed13bd0
RaresDima <rares.dima96@yahoo.com>
2017-05-22 19:31:39 +0300

    Continuation for reorganizing server functions. Optimized item lookup. pt. 2

---
+++ WebBack/ServerFunctions/Items.py
@@ -0,0 +1,37 @@
+import json
+import calendar
+
+
+def convert_todate(year, month, day):
+    year = year.split('(')[1]
+    return calendar.month_name[int(month)] + ' ' + year + ' ' + calendar.day_name[calendar.weekday(int(year), int(month), int(day))] + day
+    # transformarea unei date primite din SQL in ceva calumea
+
+
+def item_support(request_handler, querry):
+    prearranged_querry = str(querry).split(',')
+    username_querry = request_handler.db_conn.execute("select username from site_users where id=" + prearranged_querry[1])
+    username = str(username_querry).translate(None, '()[]",').translate(None, "'")
+    itemname = prearranged_querry[2].translate(None, "'")
+    return json.dumps({'item_name': itemname, 'date_posted': convert_todate(prearranged_querry[4], prearranged_querry[5],prearranged_querry[6]), 'item_description': prearranged_querry[3],'publisher': username}, indent = 4, separators = (',', ': '))
+    # un fisier json e un fisier k=cheie valoare sau vector de vectori de chei valori (lista ar fi un cuv mai bun) , but let's keep things simple
+    # general use (category items) -folosita in functii care iau iteme direct, nu folosi la altceva!
+
+
+def item_category(request_handler, category):
+    if category == "item":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Item%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "festival":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Festival%' or title LIKE '%Contest%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "ceremony":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Ceremony%' or title LIKE '%Gathering%'order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "pub":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Pub%' or title LIKE '%Cafe%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "restaurant":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Restaurant%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "hotel":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Hotel%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "party":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Party%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
+    if category == "title":
+        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Title%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
--- WebBack/ServerFunctions/__init__.py
+++ WebBack/ServerFunctions/__init__.py
@@ -1,51 +1,13 @@
 from Login        import login
 from Register     import register
+from Items        import item_category
 from SessionCheck import validate_token
 import json
-import calendar
 
-#=================================pt transformarea unei date primite din SQL in ceva calumea
-def convert_todate(an, luna, zi):
-    an_bun=an.split('(')
-    zi_num=calendar.weekday(int(an_bun[1]),int(luna),int(zi))
-    zi_name=calendar.day_name[zi_num]
-    good_date=calendar.month_name[int(luna)]+' '+an_bun[1]+' '+zi_name+zi
-    return good_date
+
 
 #==========================================================================
-#pt general use (category items) -folosita in functii care iau iteme direct, nu folosi la altceva!
-def item_support(request_handler, querry):
-    prearranged_querry = str(querry).split(',')
-    username_querry = request_handler.db_conn.execute("select username from site_users where id=" + prearranged_querry[1])
-    username = str(username_querry).translate(None, '()[]",').translate(None, "'")
-    itemname = prearranged_querry[2].translate(None, "'")
-    return json.dumps({'item_name': itemname, 'date_posted': convert_todate(prearranged_querry[4], prearranged_querry[5],prearranged_querry[6]), 'item_description': prearranged_querry[3],'publisher': username}, indent = 4, separators = (',', ': '))
-     #===============JSON POWAH!!!================================
-    #un fisier json e un fisier k=cheie valoare dupa cum probabil se si observa
-    #un fisier json poate fi si vector de vectori de chei valori (lista ar fi un cuv mai bun) , but let's keep things simple
 
-
-#============================================================================
-#============================================================================
-
-#=====pt pg de iteme======
-def item_category(request_handler, category):
-    if category == "item":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Item%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "festival":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Festival%' or title LIKE '%Contest%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "ceremony":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Ceremony%' or title LIKE '%Gathering%'order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "pub":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Pub%' or title LIKE '%Cafe%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "restaurant":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Restaurant%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "hotel":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Hotel%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "party":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Party%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
-    if category == "title":
-        return item_support(request_handler, request_handler.db_conn.execute("select * from (select * from items where title LIKE '%Title%' order by DBMS_RANDOM.RANDOM) where rownum<2"))
 
 
 #=========================
