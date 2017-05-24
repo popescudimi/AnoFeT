7a200859686f7df7b5d2f7c1090c549446edfd4f
Bogdan <bogdan.tabusca@gmail.com>
2017-05-24 20:49:23 +0300

    Recovery -login & logout work well -register (works perfectly in debugger <JScript Black Magic>) rarley works nice in normal mode <???Nu stiu momentan de ce face asta> ... -Repaired "Rares's "Optimization..." "

--- WebBack/ServerFunctions/Register.py
+++ WebBack/ServerFunctions/Register.py
@@ -1,3 +1,4 @@
+import json
 #==========================Verifiers=====================================
 #informatiile pt inregistrare sunt verificate sa fie bune
 def verify_corect_register_info(request):
@@ -26,7 +27,15 @@ def process_register_data(register_request):
 def register(request_handler, raw_request):
     print "register"
     processed_request = process_register_data(raw_request)
-    if verify_corect_register_info(processed_request) == True and verify_database_for_duplicates(request_handler, processed_request) == True:
+    if verify_corect_register_info(processed_request) == True:
+        if verify_database_for_duplicates(request_handler, processed_request) == True:
             user_id = int(request_handler.db_conn.execute("select max(id) from site_users")[0][0]) + 1
             request_handler.db_conn.callProcedure("insert_site_users", [user_id, processed_request[5], processed_request[4], processed_request[3], processed_request[0], processed_request[1], "A Question?", "Maybye"])
             print "Register successful"
+            raspuns = json.dumps({"Response": "Success"}, indent=4, separators=(',', ': '))
+            return raspuns
+        else:
+            raspuns = json.dumps({"Response": "User or Email taken"})
+            return raspuns
+    raspuns = json.dumps({"Response": "Bad Data"})
+    return raspuns
--- WebFront/JScripts/Autentificator.js
+++ WebFront/JScripts/Autentificator.js
@@ -1,6 +1,29 @@
 /**
  * Created by Bogdan on 26.03.2017.
  */
+function reg_send(un,ps,cps,em,fn,nn)
+{
+
+    var tosend="UsernameBox="+un+"&"+"Password="+ps+"&"+"ConfirmPassword="+cps+"&"+"Email="+em+"&"+"FirstName="+fn+"&"+"FamilyName="+nn;
+    var chk = new XMLHttpRequest();
+        chk.onreadystatechange = function () {
+            if (chk.readyState == 4 && chk.status == 200) {
+                if (JSON.parse(chk.response).Response == "Success") {
+                    window.location.replace("http://localhost:2526/Pages/Login.html")
+                }
+                if(JSON.parse(chk.response).Response == "User or Email taken")
+                {
+                    window.location.replace("http://localhost:2526/Pages/RegisterWait.html");
+                }
+                if(JSON.parse(chk.response).Response=="Bad Data")
+                {
+                    window.location.replace("http://localhost:2526/Pages/RegisterWait.html");
+                }
+            }
+        };
+        chk.open("POST", "User_email_pass.html", true);
+        chk.send(tosend);
+}
 function Email_bun(x)
 {
     var i = x.length;
@@ -25,36 +48,36 @@ function Email_bun(x)
 function verificare()
 {
     var sn,sp1,sp2,se,ok1=0,fn,na;
-    sn=document.getElementById('UserNAME').value;
+    sn=document.getElementById("UserNAME").value;
     sp1=document.getElementById('Parola1').value;
     sp2=document.getElementById('Parola2').value;
     se=document.getElementById('email').value;
     fn=document.getElementById('Fname').value;
-    na=document.getElementById('Name').value;
-    if(sn=="")
+    na=document.getElementById("Nname").value;
+    if(sn==="")
     {
         window.alert("Te rog completeaza campul UserNAME");
     }
     else
     {
-        if(sp1=="")
+        if(sp1==="")
         {
             window.alert("Te rog introdu o parola");
         }
         else
         {
-            if(sp2=="")
+            if(sp2==="")
                 window.alert("Te rog comfirma parola");
             else
-                if(se=="")
+                if(se==="")
                 {
                     window.alert("Email plz");
                 }
                 else
-                    if(fn.length()<3)
+                    if(fn.length<3)
                         window.alert("Introdu un First Name Valid");
                     else
-                        if (na.length()<3)
+                        if (na.length<3)
                             window.alert("Introdu un Nume Valid");
                         else
                             ok1=1;
@@ -72,7 +95,7 @@ function verificare()
                 // document.write("AUTENTIFICARE REUSITA !!! WAHOO!!!!!");
                 //window.location.replace("file:///E:/Facultate/Web/hot-orange/index.html#");
                 //send info to server
-                document.getElementById("regC").submit();
+                reg_send(sn,sp1,sp2,se,fn,na);
                 //window.location.href='86.124.39.178:2526';
 
 
@@ -82,7 +105,6 @@ function verificare()
                 window.alert("Email invalid!!!");
             }
         }
-        document.getElementById("regC").submit();
     }
 
 }
