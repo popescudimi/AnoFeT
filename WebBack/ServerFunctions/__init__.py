4a8b4571b9f26bf8ae5fd0c37bf98c995d08fa43
Bogdan <bogdan.tabusca@gmail.com>
2017-06-02 23:30:44 +0300

    The Stable Version -now you can also view your private items(and those you have been invited to) -major bug fixes in item insert and viewing maters -this version is probably the last version of the project  and the most stable one.
    -completed

--- WebBack/ServerFunctions/__init__.py
+++ WebBack/ServerFunctions/__init__.py
@@ -1,7 +1,3 @@
 from Register     import register
-from Reviews      import review_getter, review_inserter
-from Item_Search  import item_category, search_item
-from Item_Insert  import insert_item
-from Account      import public_getter
-from Reviews      import review_getter, review_inserter
-from Login        import login, validate_token
+from Items        import item_category, search_item
+from Reviews import review_getter, review_inserter
